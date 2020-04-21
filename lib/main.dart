da91146603767844a3cc2492fae27a3c5dcd6190
Manuel Cepeda <mecm1993@gmail.com>
2020-04-20 21:23:44 -0400

    refactor: main class with enums and utils
     - Modify main class to handle enums and utils
     - Minor change in buttonPressed default case

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calculator/numbers_enum.dart';
+import 'package:flutter_calculator/operators_enum.dart';
+import 'package:flutter_calculator/utils.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,50 +35,73 @@ class _MyHomePageState extends State<MyHomePage> {
   double num1 = 0.0;
   double num2 = 0.0;
   String operand = "";
+
   buttonPressed(String buttontext) {
-    //print(buttontext);
-    if (buttontext == "CLEAR") {
-      _output = "0";
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else if (buttontext == "+" ||
-        buttontext == "-" ||
-        buttontext == "/" ||
-        buttontext == "X") {
-      num1 = double.parse(output);
-      operand = buttontext;
-      _output = "0";
-    } else if (buttontext == ".") {
-      if (_output.contains(".")) {
-        print("Already contains decimal");
-        return;
-      } else {
+    CalculatorOperator value =
+        CalculatorButtonHelper.getOperatorEnum(buttontext);
+
+    switch (value) {
+      case CalculatorOperator.clear:
+        _output = "0";
+        num1 = 0.0;
+        num2 = 0.0;
+        operand = "";
+        break;
+      case CalculatorOperator.plus:
+      case CalculatorOperator.minus:
+      case CalculatorOperator.divide:
+      case CalculatorOperator.multiply:
+        num1 = double.parse(output);
+        operand = buttontext;
+        _output = "0";
+        break;
+      case CalculatorOperator.equal:
+        num2 = double.parse(output);
+        CalculatorOperator operator =
+            CalculatorButtonHelper.getOperatorEnum(operand);
+        switch (operator) {
+          case CalculatorOperator.plus:
+            _output = (num1 + num2).toString();
+            break;
+          case CalculatorOperator.minus:
+            _output = (num1 - num2).toString();
+            break;
+          case CalculatorOperator.multiply:
+            _output = (num1 * num2).toString();
+            break;
+          case CalculatorOperator.divide:
+            _output = (num1 / num2).toString();
+            break;
+          default:
+            break;
+        }
+
+        num1 = 0.0;
+        num2 = 0.0;
+        operand = "";
+
+        break;
+      case CalculatorOperator.dot:
+        if (_output.contains(
+            CalculatorButtonHelper.getOperatorValue(CalculatorOperator.dot))) {
+          print("Already contains decimal");
+          return;
+        }
+
         _output = _output + buttontext;
-      }
-    } else if (buttontext == "=") {
-      num2 = double.parse(output);
-      if (operand == "+") {
-        _output = (num1 + num2).toString();
-      }
-      if (operand == "-") {
-        _output = (num1 - num2).toString();
-      }
-      if (operand == "X") {
-        _output = (num1 * num2).toString();
-      }
-      if (operand == "/") {
-        _output = (num1 / num2).toString();
-      }
-
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else {
-      _output = _output + buttontext;
+        break;
+      default:
+        int index = _output.indexOf(
+            CalculatorButtonHelper.getOperatorValue(CalculatorOperator.dot));
+        if (index == -1) {
+          _output = _output + buttontext;
+        } else {
+          String before = _output.substring(0, index);
+          String after = _output.substring(index);
+          _output = before + buttontext + after;
+        }
+        break;
     }
-
-    print(_output);
 
     setState(() {
       output = double.parse(_output).toStringAsFixed(2);
@@ -119,32 +145,49 @@ class _MyHomePageState extends State<MyHomePage> {
               new Column(
                 children: [
                   new Row(children: [
-                    buildButton("7"),
-                    buildButton("8"),
-                    buildButton("9"),
-                    buildButton("/"),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.seven)),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.eight)),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.nine)),
+                    buildButton(CalculatorButtonHelper.getOperatorValue(
+                        CalculatorOperator.divide)),
                   ]),
                   new Row(children: [
-                    buildButton("4"),
-                    buildButton("5"),
-                    buildButton("6"),
-                    buildButton("X"),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.four)),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.five)),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.six)),
+                    buildButton(CalculatorButtonHelper.getOperatorValue(
+                        CalculatorOperator.multiply)),
                   ]),
                   new Row(children: [
-                    buildButton("1"),
-                    buildButton("2"),
-                    buildButton("3"),
-                    buildButton("-"),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.one)),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.two)),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.three)),
+                    buildButton(CalculatorButtonHelper.getOperatorValue(
+                        CalculatorOperator.minus)),
                   ]),
                   new Row(children: [
-                    buildButton("."),
-                    buildButton("0"),
+                    buildButton(CalculatorButtonHelper.getOperatorValue(
+                        CalculatorOperator.dot)),
+                    buildButton(CalculatorButtonHelper.getNumberValue(
+                        CalculatorNumber.zero)),
                     buildButton("00"),
-                    buildButton("+"),
+                    buildButton(CalculatorButtonHelper.getOperatorValue(
+                        CalculatorOperator.plus)),
                   ]),
                   new Row(children: [
-                    buildButton("CLEAR"),
-                    buildButton("="),
+                    buildButton(CalculatorButtonHelper.getOperatorValue(
+                        CalculatorOperator.clear)),
+                    buildButton(CalculatorButtonHelper.getOperatorValue(
+                        CalculatorOperator.equal)),
                   ]),
                 ],
               )
